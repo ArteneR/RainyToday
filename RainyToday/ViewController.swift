11104aab90d95d727cc66e595810734cb162ee29
Georgevici Ioan <georgevici.ioan@yahoo.com>
2016-09-08 23:04:35 +0300

    First version for testing ready

--- RainyToday/ViewController.swift
+++ RainyToday/ViewController.swift
@@ -6,17 +6,24 @@ import UIKit
 class ViewController: UIViewController {
 
    
+    @IBOutlet weak var city_UITextField: UITextField!
+    @IBOutlet weak var retrievingInfoMessage_UILabel: UILabel!
+    @IBOutlet weak var retrievedInfo_UITextView: UITextView!
     
+    @IBAction func clickedGetWeather(sender: UIButton) {
+        let city_name: String
+        city_name = city_UITextField.text!
+        print("Selected city: \(city_name)")
+        retrievingInfoMessage_UILabel.text = "Fetching information for city \(city_name)"
+        
+        self.getWeatherInfo(city_name)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        self.getWeatherInfo()
-    
     }
     
-    func getWeatherInfo() {
-        let city:String = "Timisoara"
+    func getWeatherInfo(city: String) {
         let weather = WeatherRetriever()
         
         // weather.getWeather(city)
@@ -41,6 +48,17 @@ class ViewController: UIViewController {
             print(weather.getMaxTemperature())
             
             
+            dispatch_async(dispatch_get_main_queue(), {
+                self.retrievedInfo_UITextView.text =
+                    "Code: \(weather.getWeatherCode())\n" +
+                    "Lat: \(weather.getCityLatitude())\n" +
+                    "Lon: \(weather.getCityLongitude())\n" +
+                    "Temperature: \(weather.getTemperature())\n" +
+                    "Min temperature: \(weather.getMinTemperature())\n" +
+                    "Max temperature: \(weather.getMaxTemperature())\n" +
+                    "Humidity: \(weather.getHumidity())\n" +
+                    "Pressure: \(weather.getPressure())\n"
+            })
             
         })
     
